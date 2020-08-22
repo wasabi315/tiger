91da4817f834b90d5d0eb303fc60277f254eade9
wasabi315 <wasabi.nvim.st@gmail.com>
2020-08-23 08:29:15 +0900

    format

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,8 @@
-module Main where
+module Main (main) where
 
-import Tokens (tokenize)
+import           Tokens (tokenize)
 
 main :: IO ()
 main = do
-  s <- getContents
-  print $ tokenize s
+    s <- getContents
+    print $ tokenize s
