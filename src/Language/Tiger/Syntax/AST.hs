0e901b5ee9cf5c9146d6eb0d7db5a1257d1e18a5
wasabi315 <wasabi.nvim.st@gmail.com>
2021-08-16 23:55:00 +0900

    Add AST

---
+++ src/Language/Tiger/Syntax/AST.hs
@@ -0,0 +1,100 @@
+module Language.Tiger.Syntax.AST
+  ( Expr,
+    Expr_ (..),
+    Var,
+    Var_ (..),
+    Decl (..),
+    FnDecl,
+    FnDecl_ (..),
+    VarDecl,
+    VarDecl_ (..),
+    TyDecl,
+    TyDecl_ (..),
+    Type,
+    Type_ (..),
+    Op (..),
+  )
+where
+
+import Language.Tiger.Syntax.Loc qualified as Loc
+
+type Expr = Loc.Located Expr_
+
+data Expr_
+  = Var Var
+  | Int Int
+  | Str String
+  | Call String [Expr]
+  | Op Op Expr Expr
+  | Record [Loc.Located (String, Expr)] String
+  | Seq [Expr]
+  | Assign Var Expr
+  | If Expr Expr (Maybe Expr)
+  | While Expr Expr
+  | For Var Expr Expr Expr
+  | Break
+  | Let [Decl] Expr
+  | Array String Expr Expr
+  deriving (Show)
+
+type Var = Loc.Located Var_
+
+data Var_
+  = VName String
+  | VField Var String
+  | VIxed Var Expr
+  deriving (Show)
+
+data Decl
+  = DFn [FnDecl]
+  | DVar VarDecl
+  | DTy [TyDecl]
+  deriving (Show)
+
+type FnDecl = Loc.Located FnDecl_
+
+data FnDecl_ = FnDecl
+  { fdName :: String,
+    fdParams :: [Loc.Located (String, String)],
+    fdResult :: Maybe (Loc.Located String),
+    fdBody :: Expr
+  }
+  deriving (Show)
+
+type VarDecl = Loc.Located VarDecl_
+
+data VarDecl_ = VarDecl
+  { vdName :: String,
+    vdType :: Maybe (Loc.Located String),
+    vdInit :: Expr
+  }
+  deriving (Show)
+
+type TyDecl = Loc.Located TyDecl_
+
+data TyDecl_ = TyDecl
+  { tdName :: String,
+    tdType :: Type
+  }
+  deriving (Show)
+
+type Type = Loc.Located Type_
+
+data Type_
+  = TName String
+  | TRecord
+  | TArray String
+  deriving (Show)
+
+data Op
+  = Add
+  | Sub
+  | Mul
+  | Div
+  | Eq
+  | Neq
+  | Le
+  | Lt
+  | Ge
+  | Gt
+  deriving (Show)
